b00ba12a12635110a268f4057c43eeb52431948a
Matheus Régis <matheustrai@gmail.com>
2020-07-05 16:34:54 -0300

    Create Model Account

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,12 +1,15 @@
 const express = require('express')
-
+const { Account } = require('../models')
 const router = express.Router()
 
 router.get('/sign-in', (req, res) => {
     return res.json('Sign in')
 })
 
-router.get('/sign-up', (req, res) => {
+router.get('/sign-up', async (req, res) => {
+
+    const result = await Account.create({ email: 'matheustrai@gmail.com', password: '123456' })
+    console.log(result)
     return res.json('Sign up')
 })
 
