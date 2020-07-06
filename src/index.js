d80d71d6ceee9b237559ba4d2ac1586497ddb70b
Matheus Régis <matheustrai@gmail.com>
2020-07-05 21:43:28 -0300

    Verify JWT

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const db = require('./models')
 const response =  require('./middlewares/response')
+const checkJwt =  require('./middlewares/jwt')
 
 const authController = require('./controllers/auth')
 const LinkController = require('./controllers/link')
@@ -8,6 +9,7 @@ const LinkController = require('./controllers/link')
 const app = express()
 
 app.use(response)
+app.use(checkJwt)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
