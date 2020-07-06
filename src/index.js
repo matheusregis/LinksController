f8591b3cf3b0667003942a136150b058cb73697e
Matheus Régis <matheustrai@gmail.com>
2020-07-05 21:24:47 -0300

    Links CRUD

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const db = require('./models')
 const response =  require('./middlewares/response')
 
 const authController = require('./controllers/auth')
+const LinkController = require('./controllers/link')
 
 const app = express()
 
@@ -11,6 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/auth', authController)
+app.use('/link', LinkController)
 
 app.get('/', (req, res) => {
     return res.json('Api running...')
